13dd48766c8e648ef36d23da87598acc7360faa2
nakatou <naoya.nakatou-708-1994@docomo.ne.jp>
2017-05-22 15:58:49 +0900

    Add files via upload
    0522 中東直哉

---
+++ novelMaster/js/title.js
@@ -0,0 +1,29 @@
+//タイトルシーンのキャラ選択
+//charaID = キャラ番号
+function CharaSelect(charaID){
+
+    var charaName = "";
+
+    switch (charaID) {
+        case 0:
+          charaName = "太郎君";
+          break;
+        case 1:
+          charaName = "花子さん";
+          break;
+        case 2:
+          charaName = "イチロー";
+          break;
+        default:
+          break;
+      }
+
+      //このキャラでいいか確認
+      ret = confirm(charaName + "でよろしいですか？");
+
+      //OKが押されたらページ移動
+      if(ret == true){
+        location.href = "index.html?" + charaID;
+      }
+
+  }
